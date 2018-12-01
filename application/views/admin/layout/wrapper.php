ad707a46db7fea9fe0a98ce4d3db0f4f0dfabacd
cangcimen2019 <cancgimenaan2019@gmail.com>
2018-12-01 12:49:15 +0700

    CI2

---
+++ application/views/admin/layout/wrapper.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<?php
+require_once('head.php');
+require_once('navhead.php');
+require_once('navigation.php');
+require_once('navstatic.php');
+require_once('wrapper.php');
+?>
